8c92cf73215151568de760622d94f6888b6733c4
Shreya Swaroop <shreyaswrp@gmail.com>
2020-07-16 19:44:08 +0530

    add:added  code to check the attendance of the employee

--- Employee_Wage_Computation.sh
+++ Employee_Wage_Computation.sh
@@ -1,2 +1,13 @@
 #! /bin/bash -x
 echo "Welcome to employee wage computation"
+
+result=$(($RANDOM%2))
+echo $result
+
+if [[ $result == 0 ]]
+then
+        echo "Absent"
+else
+        echo "Present"
+fi
+
